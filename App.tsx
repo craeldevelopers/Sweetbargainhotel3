1694dd4e27d274af949c35b3d2458729237d4ce6
craeldevelopers <craeldevelopers@gmail.com>
2025-07-07 18:43:31 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,46 +1,45 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "./ui-toaster";
+import { Toaster as Sonner } from "./ui-sonner";
+import { TooltipProvider } from "./ui-tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Rooms from "./pages/Rooms";
-import RoomDetails from "./pages/RoomDetails";
-import Booking from "./pages/Booking";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+
+import Index from "./Index";
+import Rooms from "./Rooms";
+import RoomDetails from "./RoomDetails";
+import Booking from "./Booking";
+import About from "./About";
+import Contact from "./Contact";
+import NotFound from "./NotFound";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const queryClient = new QueryClient();
 
 const App = () => (
-<QueryClientProvider client={queryClient}>
-<TooltipProvider>
-<Toaster />
-<Sonner />
-<BrowserRouter>
-<div className="min-h-screen flex flex-col">
-<Header />
-<main className="flex-1">
-<Routes>
-<Route path="/" element={<Index />} />
-<Route path="/rooms" element={<Rooms />} />
-<Route path="/rooms/:id" element={<RoomDetails />} />
-<Route path="/booking" element={<Booking />} />
-<Route path="/about" element={<About />} />
-<Route path="/contact" element={<Contact />} />
-<Route path="*" element={<NotFound />} />
-</Routes>
-</main>
-<Footer />
-</div>
-</BrowserRouter>
-</TooltipProvider>
-</QueryClientProvider>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Header />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/rooms" element={<Rooms />} />
+              <Route path="/rooms/:id" element={<RoomDetails />} />
+              <Route path="/booking" element={<Booking />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
-
-
