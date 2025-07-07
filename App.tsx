17df046e9b9eea8ce4304a2293045c15c53608e7
craeldevelopers <craeldevelopers@gmail.com>
2025-07-07 18:20:39 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Index from './pages/Index';
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Rooms from "./pages/Rooms";
+import RoomDetails from "./pages/RoomDetails";
+import Booking from "./pages/Booking";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Index />} />
-      </Routes>
-    </Router>
-  );
-}
+const queryClient = new QueryClient();
+
+const App = () => (
+<QueryClientProvider client={queryClient}>
+<TooltipProvider>
+<Toaster />
+<Sonner />
+<BrowserRouter>
+<div className="min-h-screen flex flex-col">
+<Header />
+<main className="flex-1">
+<Routes>
+<Route path="/" element={<Index />} />
+<Route path="/rooms" element={<Rooms />} />
+<Route path="/rooms/:id" element={<RoomDetails />} />
+<Route path="/booking" element={<Booking />} />
+<Route path="/about" element={<About />} />
+<Route path="/contact" element={<Contact />} />
+<Route path="*" element={<NotFound />} />
+</Routes>
+</main>
+<Footer />
+</div>
+</BrowserRouter>
+</TooltipProvider>
+</QueryClientProvider>
+);
 
 export default App;
+
+
